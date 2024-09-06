16c921483b36f420a1f69eb082c8b5163b00f17e
Andre <andretrevizam264@gmail.com>
2024-09-05 21:45:34 -0300

    programa não finaliza após um input invalido

--- src/desafio/Contador.java
+++ src/desafio/Contador.java
@@ -4,21 +4,27 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
+        boolean parametrosValidos = false;
 
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+        while (!parametrosValidos){
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
 
-        try {
-            //chamando o metodo contendo a lógica de contagem
-            contar(parametroUm, parametroDois);
+            try {
+                //chamando o metodo contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+                parametrosValidos = true; // Se não houver exceção, os parâmetros são válidos
 
-        }catch (ParametrosInvalidosException e) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-            System.out.println("Erro: " + e.getMessage());
+
+            }catch (ParametrosInvalidosException e) {
+                //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+                System.out.println("Erro: " + e.getMessage());
+            }
         }
 
+        terminal.close();
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
